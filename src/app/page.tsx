2d7c26710b073232ce9199fab04baaa4d844d8cc
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-10-24 15:44:58 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 "use client";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+  Stack,
+  Avatar,
+} from "@mui/material";
 import { initializeConnector } from "@web3-react/core";
 import { MetaMask } from "@web3-react/metamask";
 import { ethers } from "ethers";
 import { formatEther, parseEther } from "@ethersproject/units";
 import { useEffect, useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const [metaMask, hooks] = initializeConnector(
   (action) => new MetaMask({ actions: action })
@@ -39,25 +50,75 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <p>chainId: {chainId}</p>
-      <p>isActive: {isActive.toString()}</p>
-      <p>accounts: {accounts ? accounts : ""}</p>
-      {isActive ? (
-        <input
-          type="button"
-          onClick={handleDisconnect}
-          className="bg-neutral-300 p-2"
-          value={"Disconnect"}
-        />
-      ) : (
-        <input
-          type="button"
-          onClick={handleConnect}
-          className="bg-green-500 p-2"
-          value={"Connect"}
-        />
-      )}
-    </div>
+    <>
+      <Box sx={{ flexGrow: 1, mb: 1 }}>
+        <AppBar position="static" className="bg-white">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, color: "black" }}
+            >
+              Web3
+            </Typography>
+            <Stack direction={"row"} gap={2}>
+              {!isActivating && (
+                <Stack direction={"column"}>
+                  <Typography
+                    noWrap
+                    variant="subtitle1"
+                    sx={{ color: "black", textAlign: "center", mt: 1 }}
+                  >
+                    {accounts}
+                  </Typography>
+                </Stack>
+              )}
+
+              {isActive ? (
+                <Button
+                  title="Disconnect"
+                  type="button"
+                  variant="contained"
+                  sx={{
+                    bgcolor: "orange",
+                    color: "black",
+                  }}
+                  onClick={handleDisconnect}
+                  className="bg-transparent border-1 border-black p-2 text-black"
+                >
+                  Disconnect
+                </Button>
+              ) : (
+                <Button
+                  type="button"
+                  variant="contained"
+                  title="Connect"
+                  sx={{
+                    bgcolor: "lime",
+                  }}
+                  onClick={handleConnect}
+                  className="bg-transparent border-1 border-black text-black p-2"
+                >
+                  Connect
+                </Button>
+              )}
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div>
+        <p>chainId: {chainId}</p>
+        <p>isActive: {isActive.toString()}</p>
+        <p>accounts: {accounts ? accounts : ""}</p>
+      </div>
+    </>
   );
 }
